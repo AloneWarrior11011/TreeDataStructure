46ad80623c78a2380dc43c3e1cebcf41fb773ca2
Kunal Subhash Wadhai <123325606+AloneWarrior11011@users.noreply.github.com>
2024-04-02 15:44:05 +0530

    Create Minimum absolute difference in BST.cpp

---
+++ Minimum absolute difference in BST.cpp
@@ -0,0 +1,148 @@
+
+// Intuition -> Firstly do a inorder traversal of binary tree (in an inorder traversal's vector). 
+// then simply run loop over it find minimum difference between any two integers.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+  int data;
+  struct Node *left;
+  struct Node *right;
+};
+// Utility function to create a new Tree Node
+Node *newNode(int val)
+{
+  Node *temp = new Node;
+  temp->data = val;
+  temp->left = NULL;
+  temp->right = NULL;
+
+  return temp;
+}
+// Function to Build Tree
+Node *buildTree(string str)
+{
+  // Corner Case
+  if (str.length() == 0 || str[0] == 'N')
+    return NULL;
+
+  // Creating vector of strings from input
+  // string after spliting by space
+  vector<string> ip;
+
+  istringstream iss(str);
+  for (string str; iss >> str;)
+    ip.push_back(str);
+
+  // Create the root of the tree
+  Node *root = newNode(stoi(ip[0]));
+
+  // Push the root to the queue
+  queue<Node *> queue;
+  queue.push(root);
+
+  // Starting from the second element
+  int i = 1;
+  while (!queue.empty() && i < ip.size())
+  {
+
+    // Get and remove the front of the queue
+    Node *currNode = queue.front();
+    queue.pop();
+
+    // Get the current node's value from the string
+    string currVal = ip[i];
+
+    // If the left child is not null
+    if (currVal != "N")
+    {
+
+      // Create the left child for the current node
+      currNode->left = newNode(stoi(currVal));
+
+      // Push it to the queue
+      queue.push(currNode->left);
+    }
+
+    // For the right child
+    i++;
+    if (i >= ip.size())
+      break;
+    currVal = ip[i];
+
+    // If the right child is not null
+    if (currVal != "N")
+    {
+
+      // Create the right child for the current node
+      currNode->right = newNode(stoi(currVal));
+
+      // Push it to the queue
+      queue.push(currNode->right);
+    }
+    i++;
+  }
+
+  return root;
+}
+
+
+// } Driver Code Ends
+/*The Node structure is defined as
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+};
+*/
+
+class Solution
+{
+    public:
+    void inorder(Node *root, vector<int> &inOrder){
+        if(!root) return;
+        inorder(root->left, inOrder);
+        inOrder.push_back(root->data);
+        inorder(root->right, inOrder);
+    }
+    int absolute_diff(Node *root)
+    {
+        //Your code here
+        vector<int> inTraversal;
+        inorder(root,inTraversal);
+        int minDiff = INT_MAX;
+        for(int i = 0; i < inTraversal.size()-1; i++){
+            minDiff = min(minDiff, abs(inTraversal[i] - inTraversal[i+1]));
+        }
+        return minDiff;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main()
+{
+
+  int t;
+  scanf("%d ", &t);
+  while (t--)
+  {
+    string s;
+    getline(cin, s);
+    Node *root = buildTree(s);
+    // getline(cin, s);
+    if (root == NULL)
+      continue;
+    if (root->left == NULL and root->right == NULL)
+      continue;
+      Solution ob;
+    cout << ob.absolute_diff(root) << '\n';
+    // cout << s << '\n';
+  }
+  return 1;
+}
+// } Driver Code Ends
